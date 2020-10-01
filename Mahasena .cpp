6c4983f18eea8d0b7a6c5472a55607ed454dcecb
nidhish gaur <49364344+nidhishgaur210@users.noreply.github.com>
2020-10-01 10:19:20 +0530

    Add files via upload

---
+++ Mahasena .cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    
+    int lucky=0, unlucky=0;
+    for(int i=0; i<n; i++){
+        if(arr[i]%2==0){
+            lucky++;
+        }
+        else{
+            unlucky++;
+        }
+    }
+    if(lucky>unlucky){
+        cout<<"READY FOR BATTLE"<<endl;
+    }
+    else{
+        cout<<"NOT READY"<<endl;
+    }
+    return 0;
+}
