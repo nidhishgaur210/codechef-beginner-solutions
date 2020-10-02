a4b5c3d1ac34c980a83354bd76fc704b8edd1c3a
nidhish gaur <49364344+nidhishgaur210@users.noreply.github.com>
2020-10-02 13:43:14 +0530

    Add files via upload

---
+++ AT The Gates.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        char a[100];
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+        int h=0;
+        for(int i=n-1;i>=n-k;i--)
+        {
+            if(a[i]=='H')
+            {
+                for(int j=0;j<i;j++)
+                {
+                    if(a[j]=='H')
+                     a[j]='T';
+                    else
+                     a[j]='H';
+                }
+            }
+        }
+        for(int i=0;i<n-k;i++)
+        {
+            if(a[i]=='H')
+             h++;
+        }
+        cout<<h<<endl;
+    }
+	return 0;
+}
